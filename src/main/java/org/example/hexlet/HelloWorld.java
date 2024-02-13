5dc2b5f1c1fa8485bbe1a754605d70a767e81b0c
leomaks <s.arkhin@gmail.com>
2024-02-13 13:25:32 +0200

    update

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -9,7 +9,17 @@ public class HelloWorld {
             config.plugins.enableDevLogging();
         });
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+
+
+
+        app.get("/hello", ctx -> {
+
+            ctx.result("Hello, "
+                    +  ctx.queryParamAsClass("name", String.class).getOrDefault("World")
+                    + "!");
+        });
+
+
         app.start(7070); // Стартуем веб-сервер
     }
 }
